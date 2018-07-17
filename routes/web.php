3f77a9b8b3f66a5bc7b9e04bf0c238ce42572198
Emma Johansson Vikström <emmakristina95@hotmail.com>
2018-07-17 13:13:20 +0200

    adding controllers and routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,35 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+$router->get('/tvshows', 'TvShowsController@index');
+
+$router->get('/tvshows/{id}', 'TvShowsController@show');
+
+$router->post('/tvshows', 'TvShowsController@create');
+
+$router->put('/tvshows/{id}', 'TvShowsController@update');
+
+$router->delete('/tvshows/{id}', 'TvShowsController@destroy');
+
+
+$router->get('/showstreamer', 'StreamingServicesController@index');
+
+$router->get('/showstreamer/{id}', 'StreamingServicesController@show');
+
+$router->post('/showstreamer', 'StreamingServicesController@create');
+
+$router->put('/showstreamer/{id}', 'StreamingServicesController@update');
+
+$router->delete('/showstreamer/{id}', 'StreamingServicesController@destroy');
+
+
+$router->get('/reviews', 'ReviewsController@index');
+
+$router->get('/reviews/{id}', 'ReviewsController@show');
+
+$router->post('/reviews', 'ReviewsController@create');
+
+$router->put('/reviews/{id}', 'ReviewsController@update');
+
+$router->delete('/reviews/{id}', 'ReviewsController@destroy');
