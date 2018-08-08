36c0b00146d7b0434208f2956dfa11872cfafde5
Emma Johansson Vikström <emmakristina95@hotmail.com>
2018-08-08 18:05:14 +0200

    working on adding streaming services and redirect after adding review

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -12,7 +12,6 @@
     <!-- Scripts -->
     <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/script.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
--- resources/views/streamingservices/components/create.blade.php
+++ resources/views/streamingservices/components/create.blade.php
@@ -1,7 +1,7 @@
-<button type="button" class="btn btn-lg create-button" data-toggle="modal" data-target="#streamingModal"><i class="fa fa-plus"></i> Add new streaming service</button>
+<button type="button" class="btn btn-lg create-button" data-toggle="modal" data-target="#createModal"><i class="fa fa-plus"></i> Add new streaming service</button>
 
 <!-- Modal -->
-<div id="streamingModal" class="modal fade" role="dialog">
+<div id="createModal" class="modal fade" role="dialog">
   <div class="modal-dialog">
 
     <!-- Modal content-->
@@ -16,10 +16,10 @@
       <div class="form-group">
         <label for="title">Name</label>
         <input type="text" name="name" class="col-md-10" required />
-        <label for="title">Price</label>
-        <input type="number" name="price" class="col-md-10" required />
         <label for="title">Image</label>
         <input type="text" name="image" class="col-md-10" required />
+        <label for="title">Price</label>
+        <input type="number" name="price" class="col-md-10" required />
       </div>
       </div>
       <div class="modal-footer">
