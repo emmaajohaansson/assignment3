9ad6a5ee27ab37dc22c87afb55b0437863e7a0d6
Emma Johansson Vikström <emmakristina95@hotmail.com>
2018-08-22 22:40:31 +0200

    adding info about resources on frontpage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,12 @@
 
       <div class="img-background full-height subtract-header">
         <h1 class="frontTitle">What are you watching today?</h1>
+        <p id="startPara" class="startPage">TV Community - Connecting viewers with tv-shows</p>
+        <ul id="startList">
+          <li class="startPage">Read about tv-shows and where to find them, or add shows you want others to check out</li>
+          <li class="startPage">Get to know different streaming services and see which shows are available on different services</li>
+          <li class="startPage">Read reviews, or write your own</li>
+        </ul>
         </div>
 
 @endsection
